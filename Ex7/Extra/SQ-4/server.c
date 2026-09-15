4fd5a9a3d2423c5f02d2cb3a011ab9cc74f30b50
Vishwaa T <vishwaat27@gmail.com>
2026-09-15 07:31:22 +0530

    Implement basic multi-client chat server

---
+++ Ex7/Extra/SQ-4/server.c
@@ -0,0 +1,114 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <arpa/inet.h>
+#include <sys/socket.h>
+#include <pthread.h>
+
+#define PORT 8300
+#define MAX_CLIENTS 10
+
+int clients[MAX_CLIENTS];
+int count = 0;
+
+pthread_mutex_t lock = PTHREAD_MUTEX_INITIALIZER;
+
+void *client_handler(void *arg)
+{
+    int client = *(int *)arg;
+    int i, n;
+    char message[1024];
+
+    free(arg);
+
+    pthread_mutex_lock(&lock);
+
+    for (i = 0; i < MAX_CLIENTS; i++)
+    {
+        if (clients[i] == 0)
+        {
+            clients[i] = client;
+            break;
+        }
+    }
+
+    pthread_mutex_unlock(&lock);
+
+    while (1)
+    {
+        n = recv(client, message, sizeof(message), 0);
+
+        if (n <= 0)
+            break;
+
+        message[n] = '\0';
+
+        pthread_mutex_lock(&lock);
+
+        for (i = 0; i < MAX_CLIENTS; i++)
+        {
+            if (clients[i] != 0 && clients[i] != client)
+            {
+                send(clients[i], message, strlen(message) + 1, 0);
+            }
+        }
+
+        pthread_mutex_unlock(&lock);
+    }
+
+    pthread_mutex_lock(&lock);
+
+    for (i = 0; i < MAX_CLIENTS; i++)
+    {
+        if (clients[i] == client)
+        {
+            clients[i] = 0;
+            break;
+        }
+    }
+
+    pthread_mutex_unlock(&lock);
+
+    close(client);
+
+    printf("Client disconnected.\n");
+
+    return NULL;
+}
+
+int main()
+{
+    int server, client;
+    struct sockaddr_in address;
+    pthread_t thread;
+
+    server = socket(AF_INET, SOCK_STREAM, 0);
+
+    address.sin_family = AF_INET;
+    address.sin_addr.s_addr = INADDR_ANY;
+    address.sin_port = htons(PORT);
+
+    bind(server, (struct sockaddr *)&address, sizeof(address));
+
+    listen(server, 5);
+
+    printf("Chat Server started...\n");
+
+    while (1)
+    {
+        client = accept(server, NULL, NULL);
+
+        printf("Client connected.\n");
+
+        int *p = malloc(sizeof(int));
+        *p = client;
+
+        pthread_create(&thread, NULL, client_handler, p);
+        pthread_detach(thread);
+    }
+
+    close(server);
+
+    return 0;
+}
